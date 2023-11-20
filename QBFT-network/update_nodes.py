ef5e5d377a6dcf21dbc74ffefeea42b53563c408
root <root@DESKTOP-R64CS54>
2023-11-20 12:58:13 +0100

    Change port number

--- QBFT-network/update_nodes.py
+++ QBFT-network/update_nodes.py
@@ -12,7 +12,7 @@ def update_nodes():
     new_data = []
     for idx, node in enumerate(data):
         enode, node_ip = node.split('@')
-        node_ip = f"172.16.239.{2+idx}:{30300}?discport=0&raftport={53000}"
+        node_ip = f"172.16.239.{2+idx}:{30303}?discport=0&raftport={53000}"
         new_data.append(enode + '@' + node_ip)
     
     with open(STATIC_NODES_PATH, 'w') as f:
