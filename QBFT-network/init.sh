55374d6a401b8de20125705c8cf5257964d7cde9
Henning Hageli <hha257@DESKTOP-A5NLURJ>
2023-11-21 12:11:24 +0100

    Added option to generate bootnodes and members

--- QBFT-network/init.sh
+++ QBFT-network/init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-NUM_NODES=5
+NUM_VALIDATORS=5
+NUM_BOOTNODES=1
+NUM_MEMBERS=1
+NUM_NODES=$((NUM_VALIDATORS+NUM_BOOTNODES+NUM_MEMBERS))
 
 # For each node, create a directory "Node-{i}/data/keystore"
 
@@ -11,7 +14,7 @@ done
 
 # Create genesis file and node keys
 npm install quorum-genesis-tool
-npx quorum-genesis-tool --consensus qbft --chainID 1337 --blockperiod 5 --requestTimeout 10 --epochLength 30000 --difficulty 1 --gasLimit '0xFFFFFF' --coinbase '0x0000000000000000000000000000000000000000' --validators $NUM_NODES --members 0 --bootnodes 0 --outputPath 'artifacts'
+npx quorum-genesis-tool --consensus qbft --chainID 1337 --blockperiod 5 --requestTimeout 10 --epochLength 30000 --difficulty 1 --gasLimit '0xFFFFFF' --coinbase '0x0000000000000000000000000000000000000000' --validators $NUM_VALIDATORS --members $NUM_MEMBERS --bootnodes $NUM_BOOTNODES --outputPath 'artifacts'
 
 # Move generated files to the correct directories
 echo "Moving generated files to the correct directories"
@@ -25,13 +28,30 @@ python3 update_nodes.py
 echo "Copying the static-nodes.json file to the permissioned-nodes.json file"
 cp artifacts/goQuorum/static-nodes.json artifacts/goQuorum/permissioned-nodes.json
 
+
 # For each validator, copy files to the correct directories
 echo "Copying files to the correct directories"
-for ((i=0; i<$NUM_NODES; i++))
+for ((i=0; i<$NUM_VALIDATORS; i++))
 do
     cp artifacts/goQuorum/static-nodes.json artifacts/goQuorum/genesis.json Node-$i/data
     cp artifacts/validator$i/nodekey* artifacts/validator$i/address Node-$i/data
     cp artifacts/validator$i/account* Node-$i/data/keystore
+done
+
+# For each member, copy files to the correct directories
+for ((i=$NUM_VALIDATORS, j=0; i<$NUM_VALIDATORS+$NUM_MEMBERS; i++, j++))
+do
+    cp artifacts/goQuorum/static-nodes.json artifacts/goQuorum/genesis.json Node-$i/data
+    cp artifacts/member$j/nodekey* artifacts/member$j/address Node-$i/data
+    cp artifacts/member$j/account* Node-$i/data/keystore
+done
+
+# For each bootnode, copy files to the correct directories
+for ((i=$NUM_VALIDATORS+$NUM_MEMBERS, j=0; i<$NUM_NODES; i++, j++))
+do
+    cp artifacts/goQuorum/static-nodes.json artifacts/goQuorum/genesis.json Node-$i/data
+    cp artifacts/bootnode$j/nodekey* artifacts/bootnode$j/address Node-$i/data
+    cp artifacts/bootnode$j/account* Node-$i/data/keystore
 done
 
 # In each node directory, initialize the node
@@ -43,25 +63,8 @@ do
     cd ..
 done
 
-export ADDRESS=$(grep -o '"address": *"[^"]*"' Node-0/data/keystore/accountKeystore | grep -o '"[^"]*"$' | sed 's/"//g')
-export PRIVATE_CONFIG=ignore
-
-# Create start_node_0.sh for node 0
-cd Node-0
-echo "geth --datadir data \\
-    --networkid 1337 --nodiscover --verbosity 5 \\
-    --syncmode full \\
-    --istanbul.blockperiod 5 --mine --miner.threads 1 --miner.gasprice 0 --emitcheckpoints \\
-    --http --http.addr 127.0.0.1 --http.port 22000 --http.corsdomain \"*\" --http.vhosts \"*\" \\
-    --ws --ws.addr 127.0.0.1 --ws.port 32000 --ws.origins \"*\" \\
-    --http.api admin,eth,debug,miner,net,txpool,personal,web3,istanbul \\
-    --ws.api admin,eth,debug,miner,net,txpool,personal,web3,istanbul \\
-    --unlock 0x${ADDRESS} --allow-insecure-unlock --password ./data/keystore/accountPassword \\
-    --port 30300" > start_node_0.sh
-cd ..
-
-# Create start_node_{i}.sh for all other nodes
-for ((i=1; i<$NUM_NODES; i++))
+# Create start_node_{i}.sh for all nodes
+for ((i=0; i<$NUM_NODES; i++))
 do
     export ADDRESS=$(grep -o '"address": *"[^"]*"' Node-$i/data/keystore/accountKeystore | grep -o '"[^"]*"$' | sed 's/"//g')
     HTTP_PORT=$((22000+$i))
