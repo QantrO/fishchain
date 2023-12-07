88c02704d02461b54d0e676556d5b657784814ad
root <root@DESKTOP-R64CS54>
2023-12-07 21:09:18 +0100

    Add supprt to generate compose file with validators, members and bootnodes

--- QBFT-network/generate_docker_compose.sh
+++ QBFT-network/generate_docker_compose.sh
@@ -10,7 +10,7 @@ IMAGE_NAME="quorumengineering/quorum"
 PORT_8545=20000
 PORT_8546=20001
 NETWORK_NAME=quorum-test-net
-
+IP_ADDRESS_FILE=""
 CONTENT="---
 version: '3.6'
 
@@ -25,95 +25,113 @@ networks:
 
 services:
 "
-for ((i=0; i<$NUM_VALIDATORS; i++))
+
+# For each validator, copy files to the correct directories
+for ((i=0,  j=0; i<$NUM_VALIDATORS; i++, j++))
 do
     PORT_8545="$(expr $PORT_8546 + 1)"
     PORT_8546="$(expr $PORT_8546 + 2)"
-    DATA_STORAGE="./$NODE_NAME$i/data/:/data"
+    DATA_STORAGE="./nodes/validator-$j/data/:/data"
     IP_ADDRESS_SUFFIX=$(($i + 2))
     
     node="  
-    "validator-"$i:
-      container_name: "validator-"$i
+    "validator-"$j:
+      container_name: "validator-"$j
       image: $IMAGE_NAME
       ports:
         - $PORT_8545:8545/tcp
         - $PORT_8546:8546/tcp
         - 30303
+        - 9545
+
       entrypoint: ./docker-entrypoint.sh
       environment:
         - PRIVATE_CONFIG=ignore
       volumes:
         - ./docker-entrypoint.sh:/docker-entrypoint.sh
+        - ./scripts/add_member.sh:/data/add_member.sh
         - $DATA_STORAGE
       networks:
         quorum-test-net:
             ipv4_address: "$IP_ADDRESS$IP_ADDRESS_SUFFIX"
 "
-    #append node to docker-compose.yml
-    CONTENT+=$node
-    echo "$CONTENT" > "./docker-compose.yml"
-done
-
-for ((i=$NUM_VALIDATORS, j=0; i<$NUM_VALIDATORS+$NUM_MEMBERS; i++, j++))
-do
-    PORT_8545="$(expr $PORT_8546 + 1)"
-    PORT_8546="$(expr $PORT_8546 + 2)"
-    DATA_STORAGE="./$NODE_NAME$i/data/:/data"
-    IP_ADDRESS_SUFFIX=$(($i + 2))
-    
-    node="  
-    "member-"$i:
-      container_name: "member-"$i
-      image: $IMAGE_NAME
-      ports:
-        - $PORT_8545:8545/tcp
-        - $PORT_8546:8546/tcp
-        - 30303
-      entrypoint: ./docker-entrypoint.sh
-      environment:
-        - PRIVATE_CONFIG=ignore
-      volumes:
-        - ./docker-entrypoint.sh:/docker-entrypoint.sh
-        - $DATA_STORAGE
-      networks:
-        quorum-test-net:
-            ipv4_address: "$IP_ADDRESS$IP_ADDRESS_SUFFIX"
-
-"
+    #append ip address to ip address file
+    IP_ADDRESS_FILE+="validator-$j:$IP_ADDRESS$IP_ADDRESS_SUFFIX\n"
+    echo -e "$IP_ADDRESS_FILE" > "./node_ip_addresses.txt"
     #append node to docker-compose.yml
     CONTENT+=$node
     echo "$CONTENT" > "./docker-compose.yml"
 done
 
 # For each bootnode, copy files to the correct directories
-for ((i=$NUM_VALIDATORS+$NUM_MEMBERS, j=0; i<$NUM_NODES; i++, j++))
+for ((i=$NUM_VALIDATORS, j=0; i<$NUM_VALIDATORS+$NUM_BOOTNODES; i++, j++))
 do
     PORT_8545="$(expr $PORT_8546 + 1)"
     PORT_8546="$(expr $PORT_8546 + 2)"
-    DATA_STORAGE="./$NODE_NAME$i/data/:/data"
+    DATA_STORAGE="./nodes/bootnode-$j/data/:/data"
     IP_ADDRESS_SUFFIX=$(($i + 2))
     
     node="  
-    "bootnode-"$i:
-      container_name: "bootnode-"$i
+    "bootnode-"$j:
+      container_name: "bootnode-"$j
       image: $IMAGE_NAME
       ports:
         - $PORT_8545:8545/tcp
         - $PORT_8546:8546/tcp
         - 30303
+        - 9545
+
       entrypoint: ./docker-entrypoint.sh
       environment:
         - PRIVATE_CONFIG=ignore
       volumes:
         - ./docker-entrypoint.sh:/docker-entrypoint.sh
+        - ./scripts/add_member.sh:/data/add_member.sh
+        - $DATA_STORAGE
+      networks:
+        quorum-test-net:
+            ipv4_address: "$IP_ADDRESS$IP_ADDRESS_SUFFIX"
+"
+    #append node to docker-compose.yml
+    IP_ADDRESS_FILE+="bootnode-$j:$IP_ADDRESS$IP_ADDRESS_SUFFIX\n"
+    echo -e "$IP_ADDRESS_FILE" > "./node_ip_addresses.txt"
+    CONTENT+=$node
+    echo "$CONTENT" > "./docker-compose.yml"
+done
+
+# For each member, copy files to the correct directories
+for ((i=$NUM_VALIDATORS+$NUM_BOOTNODES, j=0; i<$NUM_NODES; i++, j++))
+do
+    PORT_8545="$(expr $PORT_8546 + 1)"
+    PORT_8546="$(expr $PORT_8546 + 2)"
+    DATA_STORAGE="./nodes/member-$j/data/:/data"
+    IP_ADDRESS_SUFFIX=$(($i + 2))
+    
+    node="  
+    "member-"$j:
+      container_name: "member-"$j
+      image: $IMAGE_NAME
+      ports:
+        - $PORT_8545:8545/tcp
+        - $PORT_8546:8546/tcp
+        - 30303
+        - 9545
+
+      entrypoint: ./docker-entrypoint.sh
+      environment:
+        - PRIVATE_CONFIG=ignore
+      volumes:
+        - ./docker-entrypoint.sh:/docker-entrypoint.sh
+        - ./scripts/add_member.sh:/data/add_member.sh
         - $DATA_STORAGE
       networks:
         quorum-test-net:
             ipv4_address: "$IP_ADDRESS$IP_ADDRESS_SUFFIX"
 
-            
 "
+    #append ip address to ip address file
+    IP_ADDRESS_FILE+="member-$j:$IP_ADDRESS$IP_ADDRESS_SUFFIX\n"
+    echo -e "$IP_ADDRESS_FILE" > "./node_ip_addresses.txt"
     #append node to docker-compose.yml
     CONTENT+=$node
     echo "$CONTENT" > "./docker-compose.yml"
