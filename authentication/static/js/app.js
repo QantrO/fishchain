0f2c09adfc6a6dc9363bb74a51f7a65911911546
Henning Hageli <henninghageli@hotmail.com>
2023-12-10 16:38:42 +0100

    Removed redundant comments

--- authentication/static/js/app.js
+++ authentication/static/js/app.js
@@ -1,5 +1,3 @@
-// If an element is hovered over, rotate it 360 degrees for 1 second
-
 document.onmouseover = function(e) {
     var target = e.target;
     target.style.transition = "transform 1s";
